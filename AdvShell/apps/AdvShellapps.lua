2f3d1698604debab4738e2397888b401b93bb2ca
kiz___ <140178579+kizYTB@users.noreply.github.com>
2025-01-26 02:13:24 +0100

    up

---
+++ AdvShell/apps/AdvShellapps.lua
@@ -0,0 +1,25 @@
+local UI = require('opus.ui')
+local Event = require('opus.event')
+
+local colors = _G.colors
+
+local page = UI.Page {
+  terminal = UI.Window {
+    x = 1, y = 1,
+    ex = -1, ey = -1,
+    backgroundColor = colors.black,
+    UI.Terminal {
+      x = 1, y = 1,
+      ex = -1, ey = -1
+    }
+  }
+}
+
+-- Lance le shell advShell dans le terminal
+function page:shell()
+  self.terminal.terminal:execute('/packages/advshell/advshell.lua')
+end
+
+UI:setPage(page)
+page:shell()
+UI:start()
